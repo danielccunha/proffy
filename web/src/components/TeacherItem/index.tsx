72c99fe28d250bf18580d75ec56b717193f60b40
Daniel Cunha <danielcunha54@gmail.com>
2020-08-04 19:17:37 -0300

    feat: Finished teacher list page UI

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import whatsappIcon from 'assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/32555455?s=460&u=ffda6cff6f9c621ab60b4aa875fc74ed6a35487c&v=4"
+          alt="Daniel Cunha"
+        />
+        <div>
+          <strong>Daniel Cunha</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Entrar em contato" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
