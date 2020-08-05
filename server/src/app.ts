5ebf9b38becf402e11c702bfa5c9098dcb4dddfe
Daniel Cunha <danielcunha54@gmail.com>
2020-08-04 21:04:42 -0300

    feat(server): Create basic express application

---
+++ server/src/app.ts
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+import routes from './routes';
+
+class App {
+  public express: express.Application;
+
+  constructor() {
+    this.express = express();
+
+    this.middleware();
+    this.database();
+    this.routes();
+  }
+
+  middleware() {
+    this.express.use(cors());
+    this.express.use(morgan('dev'));
+    this.express.use(express.json());
+  }
+
+  database() {
+    //
+  }
+
+  routes() {
+    this.express.use(routes);
+  }
+}
+
+export default new App().express;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,7 @@
-console.log('Is it working?');
+import { green } from 'chalk';
+import app from './app';
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${green(port)}`);
+});
