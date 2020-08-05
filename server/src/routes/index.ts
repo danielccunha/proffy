7892d2535747d45abaf8cf95f56e6b394a9c4e68
Daniel Cunha <danielcunha54@gmail.com>
2020-08-05 13:27:42 -0300

    feat(server): Fetch classes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,6 +4,7 @@ import * as controllers from '../controllers';
 const routes = Router();
 
 // Classes
+routes.get('/classes', controllers.Class.index);
 routes.post('/classes', controllers.Class.create);
 
 export default routes;
